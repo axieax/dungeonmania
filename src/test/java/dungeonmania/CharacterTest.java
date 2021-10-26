d32eb72f89499cac58a5a1cd4ba18ebc21f217f9
Prayag <prayag1601@gmail.com>
2021-10-26 21:05:06 +1100

    rename and move file to dungeonmania folder

--- src/test/java/dungeonmania/CharacterTest.java
+++ src/test/java/dungeonmania/CharacterTest.java
@@ -1,3 +1,4 @@
+package dungeonmania;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
