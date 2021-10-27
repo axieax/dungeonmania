943e76750ab4212745ae9c6d28775584ffdd090b
Prayag <prayag1601@gmail.com>
2021-10-27 21:11:36 +1100

    health and attack damage members are common to all moving entities - create constructors

--- src/main/java/dungeonmania/model/entities/movings/Mercenary.java
+++ src/main/java/dungeonmania/model/entities/movings/Mercenary.java
@@ -4,10 +4,15 @@ import dungeonmania.util.Direction;
 import dungeonmania.util.Position;
 
 public class Mercenary extends MovingEntity {
+    final static int MAX_MERCENARY_HEALTH = 50;
+    final static int MAX_MERCENARY_ATTACK_DMG = 5;
 
     public Mercenary(String entityId, Position position) {
-        super(entityId, position);
-        //TODO Auto-generated constructor stub
+        super(entityId, position, MAX_MERCENARY_HEALTH, MAX_MERCENARY_ATTACK_DMG);
+    }
+
+    public Mercenary(String entityId, Position position, int health, int attackDamage) {
+        super(entityId, position, health, attackDamage);
     }
 
     @Override
--- src/main/java/dungeonmania/model/entities/movings/Spider.java
+++ src/main/java/dungeonmania/model/entities/movings/Spider.java
@@ -4,10 +4,15 @@ import dungeonmania.util.Direction;
 import dungeonmania.util.Position;
 
 public class Spider extends MovingEntity {
+    final static int MAX_SPIDER_HEALTH = 20;
+    final static int MAX_SPIDER_ATTACK_DMG = 2;
 
     public Spider(String entityId, Position position) {
-        super(entityId, position);
-        //TODO Auto-generated constructor stub
+        super(entityId, position, MAX_SPIDER_HEALTH, MAX_SPIDER_ATTACK_DMG);
+    }
+
+    public Spider(String entityId, Position position, int health, int attackDamage) {
+        super(entityId, position, health, attackDamage);
     }
 
     @Override
