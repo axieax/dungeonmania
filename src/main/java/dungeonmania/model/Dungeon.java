9d862e2555dbda73e8670382711c4b033e5873a3
Andrew <x.and.i.rew.e@gmail.com>
2021-10-30 01:38:31 +1100

    added extra methods for dungeon

--- src/main/java/dungeonmania/model/Dungeon.java
+++ src/main/java/dungeonmania/model/Dungeon.java
@@ -1,53 +1,108 @@
 package dungeonmania.model;
 
-import java.util.List;
-
+import dungeonmania.EntityFactory;
 import dungeonmania.model.entities.Entity;
+import dungeonmania.model.entities.buildables.BuildableEquipment;
+import dungeonmania.model.entities.movings.Character;
 import dungeonmania.model.entities.statics.Portal;
+import dungeonmania.response.models.DungeonResponse;
 import dungeonmania.util.Position;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
-public class Dungeon {
-    public Dungeon(int width, int height) {
+public final class Dungeon {
 
+    private final String dungeonId;
+    private final String dungeonName;
+    private final List<Entity> entities = new ArrayList<>();
+
+    public Dungeon(String dungeonId, String dungeonName) {
+        this.dungeonName = dungeonName;
+        setupDungeon();
     }
 
-    public void addEntity(Entity entity) {
-
+    private final void setupDungeon() {
+        entities = EntityFactory.loadDungeon(dungeonName);
     }
 
-    public void removeEntity(Entity entity) {
-
+    public final void addEntity(Entity entity) {
+        entities.add(entity);
     }
 
-    public Entity getEntity(String entityId) {
-        return null;
+    public final boolean removeEntity(Entity entity) {
+        return entities.remove(entity);
     }
 
-    public Entity getEntityAtPosition(Position position) {
-        return null;
+    public final Entity getEntity(String entityId) {
+        return entities.stream().filter(e -> e.getId().equals(entityId)).findFirst().orElse(null);
     }
 
-    public List<Entity> getEntitiesAtPosition(Position position) {
-        return null;
+    public final Entity getCharacter() {
+        // TODO: import Character class
+        return entities.stream().filter(e -> e instanceof Character).findFirst().orElse(null);
     }
 
-    public List<Entity> getAllEntities() {
-        return null;
+    public final List<Entity> getEntities() {
+        return entities;
     }
 
-    public List<Portal> getAllPortals() {
-        return null;
+    public final List<Entity> getEntities(Position position) {
+        return entities
+            .stream()
+            .filter(e -> e.getPosition().equals(position))
+            .collect(Collectors.toList());
     }
 
-    public List<Entity> getAdjacentEntities(Position position) {
-        return null;
+    public final List<Portal> getAllPortals() {
+        return entities
+            .stream()
+            .filter(e -> e instanceof Portal)
+            .map(e -> (Portal) e)
+            .collect(Collectors.toList());
     }
 
-    public List<Entity> getCardinallyAdjacentEntities(Position position) {
-        return null;
+    public final List<Entity> getAdjacentEntities(Position position) {
+        return entities
+            .stream()
+            .filter(e -> Position.isAdjacent(e.getPosition(), position))
+            .collect(Collectors.toList());
     }
 
-    public void reachedExit() {
-        return;
+    public final List<Entity> getCardinallyAdjacentEntities(Position position) {
+        return getAdjacentEntities(position)
+            .stream()
+            .filter(e -> {
+                // cardinally adjacent if one coordinate is (1 or -1) with the other 0
+                // NOTE: can use & to extract last bit and ensure number is 0 or 1
+                // then return x XOR y
+                Position difference = Position.calculatePositionBetween(e.getPosition(), position);
+                int xDiff = Math.abs(difference.getX());
+                int yDiff = Math.abs(difference.getY());
+                if ((xDiff == 0 && yDiff == 1) || (yDiff == 0 && xDiff == 1)) return true;
+                return false;
+            })
+            .collect(Collectors.toList());
+    }
+
+    /**
+     * Returns a starter DungeonResponse object for the Dungeon
+     *
+     * @return DungeonResponse for the Dungeon
+     */
+    public final DungeonResponse getDungeonResponse() {
+        return new DungeonResponse(
+            dungeonId,
+            dungeonName,
+            entities.stream().map(Entity::getEntityResponse),
+            getCharacter().getInventoryResponse,
+            getBuildables(),
+            ""
+        );
+    }
+
+    private final List<BuildableEquipment> getBuildables() {
+        Character player = (Character) getCharacter();
+        EntityFactory.getBuildableEquipments().stream().filter(eq -> player.checkBuildable(eq));
     }
 }
