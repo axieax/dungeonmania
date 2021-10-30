8f0115ea4feaeaccf3d969c8d13856ac40c65239
Prayag <prayag1601@gmail.com>
2021-10-30 19:26:28 +1100

    delete old states

--- src/main/java/dungeonmania/model/entities/movings/MercenaryEnemyState.java
+++
@@ -1,9 +0,0 @@
-package dungeonmania.model.entities.movings;
-
-public class MercenaryEnemyState implements MercenaryState{
-    Mercenary mercenary;
-
-    public MercenaryEnemyState(Mercenary mercenary) {
-        this.mercenary = mercenary;
-    }
-}
