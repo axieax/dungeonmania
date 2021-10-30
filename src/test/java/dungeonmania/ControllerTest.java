7a5b6c657ec5d3306ccda8bd29bc24ca59bfdb9a
Amanda <amandaliu@Amandas-iMac.local>
2021-10-30 22:58:50 +1100

    added controller test functions

--- src/test/java/dungeonmania/ControllerTest.java
+++ src/test/java/dungeonmania/ControllerTest.java
@@ -6,18 +6,28 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.lang.reflect.Executable;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.spi.MarkerFactoryBinder;
 
 import dungeonmania.exceptions.InvalidActionException;
 import dungeonmania.util.Direction;
+import dungeonmania.util.Position;  
+
+import dungeonmania.response.models.DungeonResponse;
+import dungeonmania.response.models.EntityResponse;
 
 //import static dungeonmania.TestHelpers.assertListAreEqualIgnoringOrder;
 
 public class ControllerTest {
+
+    //////////////////
+    /// Test new Game
+    //////////////////
     /**
      * This test ensures that an exception is thrown when an invalid game mode
      * is given
@@ -39,6 +49,21 @@ public class ControllerTest {
     }
 
     /**
+     * This test ensures a game is created when new game is called
+     */
+    @Test
+    public void testGameIsCreated() {
+        DungeonManiaController controller = new DungeonManiaController();    
+        DungeonResponse gameResponse = controller.newGame ("advanced", "standard");
+        assertEquals(gameResponse.getDungeonName(), "advanced");
+
+    }
+
+
+    //////////////////
+    /// Test Save Game
+    //////////////////
+    /**
      * Ensures saveGame throws error if string is empty??
      */
     @Test
@@ -48,6 +73,10 @@ public class ControllerTest {
         assertThrows(IllegalArgumentException.class, () -> controller.saveGame(""));
     }
 
+
+    //////////////////
+    /// Test Load Game
+    //////////////////
     /**
      * Ensures LoadGame throws error if string is empty??
      */
@@ -56,8 +85,48 @@ public class ControllerTest {
         DungeonManiaController controller = new DungeonManiaController();  
         assertThrows(IllegalArgumentException.class, () -> controller.loadGame(""));
     }
+
+    /**
+     * Test a game persists and can be loaded.
+     */
+    @Test
+    public void testLoadGameWorks() {
+        DungeonManiaController controller = new DungeonManiaController();
+        assertDoesNotThrow(() -> controller.newGame ("advanced", "Standard"));
+
+        // player moves to battle mercenary
+        controller.tick (null, Direction.RIGHT);
+        controller.tick (null, Direction.RIGHT);
+        DungeonResponse currGame = controller.tick (null, Direction.RIGHT);
+
+        // find current position of the player
+        Position playerPosition = new Position(0, 0);
+        for (EntityResponse entity: currGame.getEntities()) {
+            if (entity.getType().equals ("player")) playerPosition = entity.getPosition();
+        }
+
+        // Save the current game
+        assertDoesNotThrow(() -> controller.saveGame ("GameOne"));
+
+        // new controller is made
+        DungeonManiaController controllerNew = new DungeonManiaController();
+        DungeonResponse loadedGame = controllerNew.loadGame("GameOne");  
+        // There should be only one goal left
+        assertEquals (loadedGame.getGoals(), "treasure");
+
+        // Player should be in the same position as when the game was saved
+        for (EntityResponse entity: loadedGame.getEntities()) {
+            if (entity.getType().equals ("player")) {
+                assertEquals(entity.getPosition(), playerPosition);
+            }
+        }
+    }
     
 
+
+    //////////////////
+    /// Test All Games
+    //////////////////
     /**
      * This function tests the persistence of games in dungeon mania
      */
@@ -65,16 +134,24 @@ public class ControllerTest {
     public void testSaveGameWorks() {
         DungeonManiaController controller = new DungeonManiaController();
         assertDoesNotThrow(() -> controller.newGame ("advanced", "Standard"));
+        
+        // Game is not saved yet, all games should contain zero games
         assertEquals(controller.allGames().size(), 0);
         
+        // Game is saved
         assertDoesNotThrow(() -> controller.saveGame ("GameOne"));
-        
-        List<String> gamesList = new ArrayList <String> ();
-        gamesList.add("GameOne");
-        // assertArrayEqualsIgnoringOrder (controller.allGames(), gamesList);
+
+        // All games should contain the saved game
         assertEquals (controller.allGames().size(), 1);
         assertTrue (controller.allGames().contains("GameOne"));
     }
+
+
+
+
+    //////////////////
+    /// Test Tick
+    //////////////////
 
     /**
      * Test an InvalidActionException is thrown if item used is not in inventory
@@ -83,6 +160,7 @@ public class ControllerTest {
     public void testItemNotInInventory () {
         DungeonManiaController controller = new DungeonManiaController();
         assertDoesNotThrow(() -> controller.newGame ("advanced", "Standard"));  
+        // invisibility poition is not in inventory
         assertThrows (InvalidActionException.class, () ->controller.tick ("invisibility_potion", Direction.NONE));    
     }
 
@@ -96,6 +174,83 @@ public class ControllerTest {
         assertDoesNotThrow(() -> controller.newGame ("advanced", "Standard"));  
         assertThrows (IllegalArgumentException.class, () ->controller.tick ("shield", Direction.NONE));    
     }
+    
+    /**
+     * Test using an invincibility potion does not throw an exception
+     */
+    /*
+    @Test
+    public void tickUsingInvincibilityPotion () {
+        DungeonManiaController controller = new DungeonManiaController();
+        assertDoesNotThrow(() -> controller.newGame ("advanced", "Standard"));
+        
+        assertDoesNotThrow(() -> controller.tick ("invincibility_potion", Direction.NONE));
+    }*/
+
+    //////////////////
+    /// Test Buildable
+    //////////////////
+    /**
+     * Tests that an item that is not a bow or shield is not buildable
+     */
+    @Test
+    public void testItemNotBuildable() {
+        DungeonManiaController controller = new DungeonManiaController();
+        assertDoesNotThrow(() -> controller.newGame ("advanced", "Standard"));  
+        assertThrows (IllegalArgumentException.class, () -> controller.build ("bad-item"));    
+    }
+
+    /**
+     * Test invalid action exception is thrown when player does not have sufficient
+     * items to craft buildable
+     */
+    @Test
+    public void testInsufficientMaterialsToBuild() {
+        DungeonManiaController controller = new DungeonManiaController();
+        assertDoesNotThrow(() -> controller.newGame ("advanced", "Standard"));  
+        assertThrows (InvalidActionException.class, () -> controller.build ("bow"));  
+        assertThrows (InvalidActionException.class, () -> controller.build ("shield"));      
+    }
+
+    /**
+     *  Test can craft buildable
+     */
+    @Test
+    public void testCanBuildObject() {
+        DungeonManiaController controller = new DungeonManiaController();
+        assertDoesNotThrow(() -> controller.newGame ("advanced", "Standard"));
+
+        for (int i = 0; i < 13; i++) {
+            controller.tick (null, Direction.DOWN);            
+        }
+
+        for (int i = 0; i < 12; i++) {
+            controller.tick (null, Direction.RIGHT);            
+        } 
+
+        for (int i = 0; i < 2; i++) {
+            controller.tick (null, Direction.LEFT);           
+        }
+
+        DungeonResponse gameResponseOne = controller.tick (null, Direction.UP);
+        assert(gameResponseOne.getBuildables().contains ("bow")); 
+
+        for (int i = 0; i < 3; i++) {
+            controller.tick (null, Direction.UP);            
+        }
+        
+        for (int i = 0; i < 4; i++) {
+            controller.tick (null, Direction.LEFT);            
+        } 
+
+        DungeonResponse gameResponseTwo = controller.tick (null, Direction.UP);
+        assert(gameResponseTwo.getBuildables().contains ("bow"));
+        assert(gameResponseTwo.getBuildables().contains ("shield")); 
+    }
+
+    //////////////////
+    /// Test Interact
+    //////////////////
 
     /**
      * Test an IllegalArgumentException is thrown if item used is not bomb,
@@ -109,6 +264,22 @@ public class ControllerTest {
     }
 
     /**
+     * Test Cannot Bribe Mercenary without money and more than two cardinal tiles away
+     */
+
+    @Test
+    public void testTooFarFromMercenary() {
+        DungeonManiaController controller = new DungeonManiaController();
+        DungeonResponse gameResponse = controller.newGame ("advanced", "Standard"); 
+        String mercenaryId = "";
+        for (EntityResponse entity : gameResponse.getEntities()) {
+            if (entity.getType().equals("mercenary")) mercenaryId = entity.getId();
+        } 
+        assertThrows (InvalidActionException.class, (mercenaryId)->controller.interact(mercenaryId));
+
+    }
+
+    /**
      * Test a player cannot bribe a mercenary if not adjacent
      */
     @Test
@@ -117,26 +288,5 @@ public class ControllerTest {
         assertDoesNotThrow(() -> controller.newGame ("advanced", "Standard"));  
         assertThrows (InvalidActionException.class, () ->controller.interact("1"));            
     }
-
-    /**
-     * Player does not have sufficient items to craft the buildable
-     */
-    @Test
-    public void testCannotBuild() {
-        DungeonManiaController controller = new DungeonManiaController();
-        assertDoesNotThrow(() -> controller.newGame ("advanced", "Standard"));  
-        assertThrows (InvalidActionException.class, () ->controller.interact("bow"));         
-    }
-
-    /**
-     * The buildable is not an invalid type (not bow or shield)
-     */
-    @Test
-    public void testInvalidBuildableType() {
-        DungeonManiaController controller = new DungeonManiaController();
-        assertDoesNotThrow(() -> controller.newGame ("advanced", "Standard"));  
-        assertThrows (IllegalArgumentException.class, () ->controller.interact("invalid-type"));            
-    }
-
 
 }
---
+++ src/test/java/dungeonmania/DungeonTest.java
@@ -0,0 +1,23 @@
+package dungeonmania;
+
+package dungeonmania;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import dungeonmania.exceptions.InvalidActionException;
+import dungeonmania.response.models.DungeonResponse;
+import dungeonmania.util.Direction;  
+
+public class DungeonTest {
+    
+}
