f34eab7dc345129d0978cd62f428d5fe790518a6
William Feng <williamfeng2001@gmail.com>
2021-10-30 21:45:48 +1100

    Added more tests for boulder now that there's a better idea of how the movement works

--- src/test/java/dungeonmania/statics/BoulderTest.java
+++ src/test/java/dungeonmania/statics/BoulderTest.java
@@ -4,7 +4,9 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import dungeonmania.model.Dungeon;
+import dungeonmania.model.entities.movings.Player;
 import dungeonmania.model.entities.statics.Boulder;
+import dungeonmania.util.Direction;
 import dungeonmania.util.Position;
 import org.junit.jupiter.api.Test;
 
@@ -22,7 +24,7 @@ public class BoulderTest {
     }
 
     /**
-     * Test if Player interacts with the boulder. If player moves to the boulder,
+     * Test if player interacts with the boulder. If player moves onto the boulder,
      * the boulder should also move.
      */
     @Test
@@ -30,15 +32,34 @@ public class BoulderTest {
         Dungeon dungeon = new Dungeon(3, 3);
         dungeon.addEntity(new Boulder("boulder1", new Position(1, 1)));
 
-        // dungeon.getEntity("boulder1").move(new Position(2, 2));
-        fail();
+        Player player = new Player("player1", new Position(2, 1));
+
+        dungeon.addEntity(player);
+        player.move(dungeon, Direction.LEFT);
+
+        // Player and boulder should both move to the left by one 
+        assertTrue(new Position(0, 1).equals(dungeon.getEntity("boulder1").getPosition()));
+        assertTrue(new Position(1, 1).equals(player.getPosition()));
     }
 
     /**
-     * Test if Player interacts with the boulder. Player cannot push more than one boulder.
+     * Test that the player cannot push more than one boulder at once.
      */
     @Test
-    public void boulderMoveByBoulder() {
-        fail();
+    public void boulderBlocksBoulderMovement() {
+        Dungeon dungeon = new Dungeon(3, 3);
+        dungeon.addEntity(new Boulder("boulder1", new Position(1, 1)));
+        dungeon.addEntity(new Boulder("boulder2", new Position(2, 1)));
+
+        Player player = new Player("player1", new Position(3, 1));
+
+        dungeon.addEntity(player);
+        player.move(dungeon, Direction.LEFT);
+
+        // Since there are two boulders next to each other
+        // Neither the player nor boulders should move
+        assertTrue(new Position(1, 1).equals(dungeon.getEntity("boulder1").getPosition()));
+        assertTrue(new Position(2, 1).equals(dungeon.getEntity("boulder2").getPosition()));
+        assertTrue(new Position(3, 1).equals(player.getPosition()));
     }
 }
