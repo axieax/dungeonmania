56d6eefb34f051895f10648b73f68e9e4126585a
Prayag <prayag1601@gmail.com>
2021-10-30 19:44:56 +1100

    add interact function and clarify update method

--- src/main/java/dungeonmania/model/entities/movings/Mercenary.java
+++ src/main/java/dungeonmania/model/entities/movings/Mercenary.java
@@ -7,6 +7,7 @@ import dungeonmania.util.Position;
 public class Mercenary extends MovingEntity implements Observer {
     public final static int MAX_MERCENARY_HEALTH = 50;
     public final static int MAX_MERCENARY_ATTACK_DMG = 5;
+    public final static int TREASURE_REQUIRED_TO_BRIBE = 1;
 
     private MercenaryState defaultState;
     private MercenaryState runState;
@@ -30,15 +31,24 @@ public class Mercenary extends MovingEntity implements Observer {
         state.move(dungeon, playerPos);
     }
 
+    /**
+     * Change state if player is invincible and mercenary not ally of player
+     */
     @Override
-    public void update(SubjectPlayer player) { }
+    public void update(SubjectPlayer player) { 
+        
+    }
 
     /** 
      * Character attempts to bribe mercenary
      */
     @Override
     public void interact(Dungeon dungeon, MovingEntityBehaviour character) { 
-        
+        Player player = (Player) character;
+
+        if(player.getTreasure() >= TREASURE_REQUIRED_TO_BRIBE) {
+            player.addAlly(this);
+        }
     }
 
     ////////////////////////////////////////////////////////////////////////////////////
