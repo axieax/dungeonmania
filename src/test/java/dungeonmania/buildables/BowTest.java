7e88454392fa9e52e65b374fb95c23b83efa8e5d
William Feng <williamfeng2001@gmail.com>
2021-11-01 10:59:44 +1100

    Created additional bow test for battle

--- src/test/java/dungeonmania/buildables/BowTest.java
+++ src/test/java/dungeonmania/buildables/BowTest.java
@@ -1,7 +1,6 @@
 package dungeonmania.buildables;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
 
 import java.util.ArrayList;
 
@@ -12,6 +11,7 @@ import dungeonmania.model.entities.collectables.Arrow;
 import dungeonmania.model.entities.collectables.Wood;
 import dungeonmania.model.entities.movings.Mercenary;
 import dungeonmania.model.entities.movings.Player;
+import dungeonmania.model.entities.statics.ZombieToastSpawner;
 import dungeonmania.model.goal.ExitCondition;
 import dungeonmania.model.mode.Mode;
 import dungeonmania.model.mode.Standard;
@@ -40,6 +40,7 @@ public class BowTest {
 
         // Player picks up the wood and arrows
         Player player = new Player(new Position(0, 0));
+        game.addEntity(player);
         player.move(game, Direction.RIGHT);
         player.move(game, Direction.DOWN);
         player.move(game, Direction.DOWN);
@@ -84,17 +85,18 @@ public class BowTest {
         Bow bow = new Bow();
         player.craft(bow);
 
-        // Durability of bow when picked up should be 5
+        // Durability of bow when crafted should be 5
         Item item = player.getInventoryItem(bow.getId());
         assertTrue(((Bow) item).getDurability() == 5);
 
-        Mercenary mercenary = new Mercenary(new Position(2, 3), mode.damageMultiplier(), player);
-        game.addEntity(mercenary);
+        ZombieToastSpawner spawner = new ZombieToastSpawner(new Position(3, 3), mode.damageMultiplier());
+        game.addEntity(spawner);
 
-        // Player moves to attack (interact with) the mercenary with the bow
-        // This will cause the durability of the bow to decrease by 1
         player.move(game, Direction.RIGHT);
 
+        // Player is now next to the zombie toast spawner and will proceed to destroy it with the bow
+        // This will cause the durability of the bow to decrease by 1
+        game.interact(spawner.getId());
         assertTrue(((Bow) item).getDurability() == 4);
     }
 
@@ -103,6 +105,44 @@ public class BowTest {
      */
     @Test
     public void battleTest() {
-        fail();
+        Mode mode = new Standard();
+        Game game = new Game("game", new ArrayList<>(), new ExitCondition(), mode);
+
+        Wood wood = new Wood(new Position(1, 0));
+        Arrow arrow1 = new Arrow(new Position(1, 1));
+        Arrow arrow2 = new Arrow(new Position(1, 2));
+        Arrow arrow3 = new Arrow(new Position(1, 3));
+
+        game.addEntity(wood);
+        game.addEntity(arrow1);
+        game.addEntity(arrow2);
+        game.addEntity(arrow3);
+
+        Player player = new Player(new Position(0, 0));
+        player.move(game, Direction.RIGHT);
+        player.move(game, Direction.DOWN);
+        player.move(game, Direction.DOWN);
+        player.move(game, Direction.DOWN);
+
+        Bow bow = new Bow();
+        player.craft(bow);
+
+        Item item = player.getInventoryItem(bow.getId());
+        assertTrue(((Bow) item).getDurability() == 5);
+
+        // Mercenary spawns with 50 health
+        Mercenary mercenary = new Mercenary(new Position(2, 3), mode.damageMultiplier(), player);
+        game.addEntity(mercenary);
+
+        assertTrue(mercenary.getHealth() == 50);
+
+        // Player moves to attack (interact with) the mercenary with the bow
+        // Upon attacking the player will inflict 10 + 30 * 2 = 70 damage to the mercenary
+        // This means the mercenary will be killed and is removed from Game
+        // The durability of the bow will also decrease by 1
+        player.move(game, Direction.RIGHT);
+
+        assertTrue(game.getEntity(mercenary.getId()) == null);
+        assertTrue(((Bow) item).getDurability() == 4);
     }
 }
