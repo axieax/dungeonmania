b575f740905f419f6f92c4d49e8e28af92182531
Prayag <prayag1601@gmail.com>
2021-11-09 22:57:45 +1100

    triple attack damage if attacking any bosses

--- src/main/java/dungeonmania/model/entities/collectables/equipment/Anduril.java
+++ src/main/java/dungeonmania/model/entities/collectables/equipment/Anduril.java
@@ -1,6 +1,10 @@
 package dungeonmania.model.entities.collectables.equipment;
 
 import dungeonmania.model.entities.AttackEquipment;
+import dungeonmania.model.entities.Entity;
+import dungeonmania.model.entities.movings.Boss;
+import dungeonmania.model.entities.movings.Hydra;
+import dungeonmania.model.entities.movings.player.Player;
 import dungeonmania.util.Position;
 
 public class Anduril extends AttackEquipment {
@@ -12,4 +16,15 @@ public class Anduril extends AttackEquipment {
         super("anduril", ATTACK_DAMAGE, HIT_RATE, position);
     }
 
+    @Override
+    public double useEquipment(Player player, Entity enemy) {
+        double attackAmount = super.useEquipment(player, enemy);
+        
+        // A very high damage sword which causes triple damage against bosses
+        if(enemy instanceof Boss) {
+            attackAmount = attackAmount * 3;
+        }
+
+        return attackAmount;
+    }
 }
