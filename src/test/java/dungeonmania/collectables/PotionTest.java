de579a16e754eb6fabf4363685cb52ff0b6056e4
Gabriel Ting <65492346+gtangelo@users.noreply.github.com>
2021-11-12 13:46:11 +1100

    fixed zombie test

--- src/test/java/dungeonmania/collectables/PotionTest.java
+++ src/test/java/dungeonmania/collectables/PotionTest.java
@@ -147,9 +147,9 @@ public class PotionTest {
 
         game.tick(null, Direction.NONE);
         assertTrue(new Position(7, 0).equals(zombie.getPosition())); 
+        assertTrue(zombie.getMovementState() instanceof RunMovementState); 
 
         game.tick(null, Direction.NONE);
-        assertTrue(new Position(8, 0).equals(zombie.getPosition())); 
         // after 3 ticks, zombie should be in random state
         assertTrue(zombie.getMovementState() instanceof RandomMovementState);        
     }
