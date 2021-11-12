ab880f92bde9e44afe5bfed758e468ac63c04281
William Feng <williamfeng2001@gmail.com>
2021-11-12 13:59:06 +1100

    Javadoc and new method for checking there are no zombies in the dungeon

--- src/main/java/dungeonmania/model/entities/buildables/Buildable.java
+++ src/main/java/dungeonmania/model/entities/buildables/Buildable.java
@@ -1,12 +1,34 @@
 package dungeonmania.model.entities.buildables;
 
+import dungeonmania.model.Game;
 import dungeonmania.model.entities.movings.player.Inventory;
 
 public interface Buildable {
     
+    /**
+     * Check if the equipment is buildable.
+     * @param inventory
+     * @return true if the player has enough resources to build the object.
+     */
     public abstract boolean isBuildable(Inventory inventory);
 
+    /**
+     * Check if there are any zombies currently in the dungeon.
+     * @param game
+     * @param inventory
+     * @return true if there are no zombies in the dungeon.
+     */
+    public abstract boolean checkNoZombies(Game game, Inventory inventory);
+
+    /**
+     * Remove the resources used for building from the player's inventory.
+     * @param inventory
+     */
     public abstract void craft(Inventory inventory);
 
+    /**
+     * Create a copy of the object for the Entity Factory.
+     * @return an instance of the equipment
+     */
     public abstract Buildable clone();
 }
