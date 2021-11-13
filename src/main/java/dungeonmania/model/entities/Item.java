b0870cf77716b1b452bdc20e5406cef44f953b53
William Feng <williamfeng2001@gmail.com>
2021-11-13 22:30:40 +1100

    Unnecessary methods never used in the item class

--- src/main/java/dungeonmania/model/entities/Item.java
+++ src/main/java/dungeonmania/model/entities/Item.java
@@ -2,21 +2,12 @@ package dungeonmania.model.entities;
 
 import dungeonmania.model.Game;
 import dungeonmania.model.entities.movings.player.Player;
-import dungeonmania.response.models.ItemResponse;
 import dungeonmania.util.Position;
 
 public abstract class Item extends Entity {
 
     public Item(String prefix, Position position) {
         super(prefix, position, false, true);
-    }
-    
-    public Item(String prefix) {
-        this(prefix, null);
-    }
-
-    public ItemResponse getItemInfo() {
-        return new ItemResponse(this.getId(), this.getClass().getSimpleName());
     }
 
     /**
