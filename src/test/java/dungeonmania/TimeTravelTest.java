5efb38d57c583263be70a136995bd964908d1d94
Gabriel Ting <65492346+gtangelo@users.noreply.github.com>
2021-11-13 13:57:29 +1100

    added time turner tests

--- src/test/java/dungeonmania/TimeTravelTest.java
+++ src/test/java/dungeonmania/TimeTravelTest.java
@@ -29,6 +29,51 @@ public class TimeTravelTest {
         return inventory.stream().filter(item -> item.getType().equals("time_turner")).count() > 0;
     }
 
+    private DungeonResponse goToTimeTurnerFromSpawnPoint(DungeonManiaController dmc) {
+        DungeonResponse resp = null;
+        for (int i = 0; i < 2; ++i) resp = dmc.tick(null, Direction.DOWN);
+        return resp;
+    }
+
+    private DungeonResponse goToTimeTravellingPortal1FromSpawnPoint(DungeonManiaController dmc) {
+        for (int i = 0; i < 2; ++i) assertDoesNotThrow(() -> dmc.tick(null, Direction.DOWN));
+        for (int i = 0; i < 2; ++i) assertDoesNotThrow(() -> dmc.tick(null, Direction.RIGHT));
+        for (int i = 0; i < 2; ++i) assertDoesNotThrow(() -> dmc.tick(null, Direction.DOWN));
+        for (int i = 0; i < 6; ++i) assertDoesNotThrow(() -> dmc.tick(null, Direction.DOWN));
+        // enter time travel portal
+        return dmc.tick(null, Direction.UP);
+    }
+
+    /**
+     * Returns the old player
+     *
+     * @param entities entities from DungeonResponse
+     *
+     * @return old player response, or null if old player does not exist
+     */
+    private EntityResponse getOldPlayer(List<EntityResponse> entities) {
+        return entities
+            .stream()
+            .filter(e -> e.getType().equals("older_player"))
+            .findFirst()
+            .orElse(null);
+    }
+
+    /**
+     * Returns the player
+     *
+     * @param entities entities from DungeonResponse
+     *
+     * @return player response, or null if old player does not exist
+     */
+    private EntityResponse getPlayer(List<EntityResponse> entities) {
+        return entities
+            .stream()
+            .filter(e -> e.getType().equals("player"))
+            .findFirst()
+            .orElse(null);
+    }
+
     /**
      * Returns the position of the player
      *
@@ -82,7 +127,7 @@ public class TimeTravelTest {
         // create game and collect time turner
         DungeonManiaController dmc = new DungeonManiaController();
         dmc.newGame("hourglass", "standard");
-        for (int i = 0; i < 2; ++i) dmc.tick(null, Direction.DOWN);
+        goToTimeTurnerFromSpawnPoint(dmc);
 
         // invalid ticks
         assertThrows(IllegalArgumentException.class, () -> dmc.rewind(0));
@@ -94,8 +139,7 @@ public class TimeTravelTest {
         // create game and collect time turner
         DungeonManiaController dmc = new DungeonManiaController();
         dmc.newGame("hourglass", "standard");
-        DungeonResponse resp = null;
-        for (int i = 0; i < 2; ++i) resp = dmc.tick(null, Direction.DOWN);
+        DungeonResponse resp = goToTimeTurnerFromSpawnPoint(dmc);
         assertEquals(new Position(1, 13), getPlayerPosition(resp.getEntities()));
 
         // go back one position
@@ -109,7 +153,7 @@ public class TimeTravelTest {
         // create game and collect time turner
         DungeonManiaController dmc = new DungeonManiaController();
         DungeonResponse initialState = dmc.newGame("hourglass", "standard");
-        for (int i = 0; i < 2; ++i) dmc.tick(null, Direction.DOWN);
+        goToTimeTurnerFromSpawnPoint(dmc);
 
         // try to rewind more than 2 moves
         assertDoesNotThrow(() -> {
@@ -124,13 +168,106 @@ public class TimeTravelTest {
         // create game and collect time turner
         DungeonManiaController dmc = new DungeonManiaController();
         DungeonResponse initialState = dmc.newGame("hourglass", "standard");
-        for (int i = 0; i < 2; ++i) dmc.tick(null, Direction.DOWN);
+        goToTimeTurnerFromSpawnPoint(dmc);
 
         // try to rewind 2 moves
         assertDoesNotThrow(() -> {
             DungeonResponse resp = dmc.rewind(2);
             assertEquals(new Position(1, 13), getPlayerPosition(resp.getEntities()));
             assertEquals(new Position(1, 11), getOldPlayerPosition(resp.getEntities()));
+        });
+    }
+
+    /**
+     * Test rewind with old player following set path using time turner (1 tick)
+     */
+    @Test
+    public void testRewindOneTickOldPlayerMovement() {
+        // create game and collect time turner
+        DungeonManiaController dmc = new DungeonManiaController();
+        DungeonResponse initialState = dmc.newGame("hourglass", "standard");
+        goToTimeTurnerFromSpawnPoint(dmc);
+        assertDoesNotThrow(() -> dmc.tick(null, Direction.RIGHT));
+
+        // try to rewind 1 move
+        assertDoesNotThrow(() -> {
+            DungeonResponse resp = dmc.rewind(1);
+            assertEquals(new Position(2, 13), getPlayerPosition(resp.getEntities()));
+            assertEquals(new Position(1, 13), getOldPlayerPosition(resp.getEntities()));
+
+            // checks past player moves right 
+            resp = dmc.tick(null, Direction.RIGHT);
+            assertEquals(new Position(2, 13), getOldPlayerPosition(resp.getEntities()));
+            assertEquals(new Position(3, 13), getPlayerPosition(resp.getEntities()));
+
+            // old player should disappear
+            resp = dmc.tick(null, Direction.RIGHT);
+            assertEquals(null, getOldPlayer(resp.getEntities()));
+            assertEquals(new Position(4, 13), getPlayerPosition(resp.getEntities()));
+        });
+    }
+
+    /**
+     * Test rewind with old player following set path using time turner (5 ticks)
+     */
+    @Test
+    public void testRewindFiveTicksOldPlayerMovement() {
+        // create game and collect time turner
+        DungeonManiaController dmc = new DungeonManiaController();
+        DungeonResponse initialState = dmc.newGame("hourglass", "standard");
+        goToTimeTurnerFromSpawnPoint(dmc);
+        assertDoesNotThrow(() -> dmc.tick(null, Direction.RIGHT));
+        assertDoesNotThrow(() -> dmc.tick(null, Direction.RIGHT));
+        assertDoesNotThrow(() -> dmc.tick(null, Direction.UP));
+        assertDoesNotThrow(() -> dmc.tick(null, Direction.DOWN));
+        assertDoesNotThrow(() -> dmc.tick(null, Direction.RIGHT));
+
+        // try to rewind 5 move
+        assertDoesNotThrow(() -> {
+            DungeonResponse resp = dmc.rewind(5);
+            assertEquals(new Position(4, 13), getPlayerPosition(resp.getEntities()));
+            assertEquals(new Position(1, 13), getOldPlayerPosition(resp.getEntities()));
+
+            // checks past player movements 
+            resp = dmc.tick(null, Direction.NONE);
+            assertEquals(new Position(2, 13), getOldPlayerPosition(resp.getEntities()));
+            resp = dmc.tick(null, Direction.NONE);
+            assertEquals(new Position(3, 13), getOldPlayerPosition(resp.getEntities()));
+            resp = dmc.tick(null, Direction.NONE);
+            assertEquals(new Position(3, 12), getOldPlayerPosition(resp.getEntities()));
+            resp = dmc.tick(null, Direction.NONE);
+            assertEquals(new Position(3, 13), getOldPlayerPosition(resp.getEntities()));
+
+            // old player be on the tile where the current player used the time turner
+            Position currPos = getPlayerPosition(resp.getEntities());
+            resp = dmc.tick(null, Direction.RIGHT);
+            assertEquals(currPos, getOldPlayerPosition(resp.getEntities()));
+
+            // old player should now disappear
+            resp = dmc.tick(null, Direction.NONE);
+            assertEquals(null, getOldPlayer(resp.getEntities()));
+        });
+    }
+
+    /**
+     * Test rewind with old player and initiate a battle
+     */
+    @Test
+    public void testRewindBattle() {
+        // create game and collect time turner
+        DungeonManiaController dmc = new DungeonManiaController();
+        DungeonResponse initialState = dmc.newGame("hourglass", "standard");
+        goToTimeTurnerFromSpawnPoint(dmc);
+
+        // try to rewind 1 move
+        assertDoesNotThrow(() -> {
+            DungeonResponse resp = dmc.rewind(1);
+            assertEquals(new Position(1, 13), getPlayerPosition(resp.getEntities()));
+            assertEquals(new Position(1, 12), getOldPlayerPosition(resp.getEntities()));
+
+            // past player starts battle with current player
+            resp = dmc.tick(null, Direction.NONE);
+            assertTrue(getOldPlayer(resp.getEntities()) == null || getPlayer(resp.getEntities()) == null);
         });
     }
 }
